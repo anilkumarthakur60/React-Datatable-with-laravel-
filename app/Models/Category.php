32ae4aa95a907da159a6e21bafd04fdf932b45b2
Anil <anilkumarthakur60@gmail.com>
2022-12-17 22:22:45 +0545

    update

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,28 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     use HasFactory;
+    use Sluggable;
 
-    public function posts():HasMany
+    public function posts(): HasMany
     {
         return $this->hasMany(Post::class);
     }
 
-      use Sluggable;
 
-       public function sluggable(): array
-          {
-              return [
-                  'slug' => [
-                      'source' => 'name'
-                  ]
-              ];
-          }
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name',
+            ],
+        ];
+    }
 }
