d6b463dc3307c9135c7b4eb66851c398c09f1051
Anil <anilkumarthakur60@gmail.com>
2022-12-20 01:03:15 +0545

    update

--- resources/js/Pages/Posts/Indexs.tsx
+++ resources/js/Pages/Posts/Indexs.tsx
@@ -3,14 +3,14 @@ import AppLayout from '@/Layouts/AppLayout';
 import DataTable from 'react-data-table-component';
 import useRoute from '@/Hooks/useRoute';
 import axios from "axios";
-import styled from 'styled-components';
 import {set} from "lodash";
+import {data} from "autoprefixer";
 
 export default function Index() {
 
-    const [datas, setData] = useState([]);
+    const [data, setData] = useState([]);
     const [page, setPage] = useState(1);
-    const [sort, setSort] = useState('id');
+    const [sort, setSort] = useState('name');
     const [sortDir, setSortDir] = useState('asc');
     const [filters, setFilters] = useState({});
     const [loading, setLoading] = useState(false);
@@ -20,9 +20,6 @@ export default function Index() {
     const route = useRoute();
 
     const getData = async page => {
-console.log('---------data logging while calling api--------',page);
-        try {
-            setLoading(true);
             const response = await axios.get(route('posts.create',
                 {
                     page: page,
@@ -35,11 +32,7 @@ console.log('---------data logging while calling api--------',page);
                 }));
             setData(response.data.data);
             setTotalRows(response.data.meta.total);
-            setLoading(false);
 
-        } catch (e) {
-            console.log(e);
-        }
     }
 
 
@@ -81,32 +74,58 @@ console.log('---------data logging while calling api--------',page);
     ];
 
     const handleSort = async (column, sortDirection) => {
-        console.log('---------data logging- handleSort-------', column, sortDirection);
-
-        setLoading(true);
         setSort(column.sortField);
         setSortDir(sortDirection);
         getData(page);
-        setLoading(false);
     };
-    const handlePerRowsChange = async (newPerPage, page) => {
-        console.log('---------data logging- handlePerRowsChange-------', newPerPage, page);
+    const handleRowPerPageChange = async (newPerPage, page) => {
 
         setPerPage(newPerPage);
-        getData(newPerPage);
-        setLoading(false);
+        setPage(page);
+        const response = await axios.get(route('posts.create',
+            {
+                page: page,
+                search: search,
+                sort: sort,
+                sortDir: sortDir,
+                filters: filters,
+                per_page: newPerPage
+
+            }));
+        setData(response.data.data);
+        setTotalRows(response.data.meta.total);
+
     };
     const handlePageChange = async page => {
-        console.log('---------data logging- handlePageChange-------', page);
-        setLoading(true);
         setPage(page);
         getData(page);
-        setLoading(false);
     }
 
     useEffect(() => {
+            getData(1);
+        },
+        []);
+
+    const resetAll = () => {
+        setFilters({});
+        setSearch('');
+
+        setPage(1);
+        setSort('name');
+        setSortDir('asc');
+        setPerPage(10);
         getData(1);
-    }, []);
+
+    }
+    const paginationOptions ={
+            rowsPerPageText: 'Rows per page:',
+            rangeSeparatorText: 'off',
+            noRowsPerPage: false,
+            selectAllRowsItem: true,
+            selectAllRowsItemText: 'All',
+
+
+        }
 
 
     return (
@@ -118,10 +137,15 @@ console.log('---------data logging while calling api--------',page);
                 </h2>
             )}
         >
+            <button type="button" onClick={resetAll}
+                    className="py-2 px-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+                Delete
+            </button>
             <div className="py-12">
                 <DataTable
                     columns={columns}
-                    data={datas}
+                    data={data}
 
 
                     progressPending={loading}
@@ -132,11 +156,12 @@ console.log('---------data logging while calling api--------',page);
                     onSort={handleSort}
                     sortServer
                     sortIcon={<i className="fas fa-sort"></i>}
-                    onChangeRowsPerPage={handlePerRowsChange}
+                    onChangeRowsPerPage={handleRowPerPageChange}
                     onChangePage={handlePageChange}
                     fixedHeader={true}
                     fixedHeaderScrollHeight="100vh"
                     highlightOnHover={true}
+                    paginationComponentOptions={paginationOptions}
 
 
                 />
